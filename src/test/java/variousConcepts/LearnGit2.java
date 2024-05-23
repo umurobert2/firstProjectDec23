0a4e0fd88e8ff4c32a803b530f71e330e6811e29
umurobert2 <83524433+umurobert2@users.noreply.github.com>
2024-05-23 03:14:19 -0500

    Learn git 2

---
+++ src/test/java/variousConcepts/LearnGit2.java
@@ -0,0 +1,24 @@
+package variousConcepts;
+
+public class LearnGit2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	public void printMe1(){
+		 System.out.println(" branch 2 : learning Github is fun ");
+	}
+	public void printMe2(){
+		 System.out.println(" branch 2 : learning Github is fun ");
+	}
+	public void printMe3(){
+		 System.out.println(" branch 2 : learning Github is fun ");
+	}
+	public void printMe4(){
+		 System.out.println(" branch 2 : learning Github is fun ");
+	}
+
+}
